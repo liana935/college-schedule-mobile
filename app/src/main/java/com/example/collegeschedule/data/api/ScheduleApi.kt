5dc7a9e7a75a634719213dba0e5770cd2c349201
Podstrelov Slava <podstrel385@gmail.com>
2026-02-05 20:01:31 +0700

    Added getAllGroups method

--- app/src/main/java/com/example/collegeschedule/data/api/ScheduleApi.kt
+++ app/src/main/java/com/example/collegeschedule/data/api/ScheduleApi.kt
@@ -1,6 +1,7 @@
 package com.example.collegeschedule.data.api
 
 import com.example.collegeschedule.data.dto.ScheduleByDateDto
+import com.example.collegeschedule.data.dto.GroupDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,4 +14,7 @@ interface ScheduleApi {
         @Query("start") start: String,
         @Query("end") end: String
     ): List<ScheduleByDateDto>
+
+    @GET("api/groups")
+    suspend fun getAllGroups(): List<GroupDto>
 }
